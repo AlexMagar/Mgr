18a18fd9d6d01e42578dbcf8103949b84a18d1d5
AlexMagar <lx.mgr07@gmail.com>
2022-06-07 20:57:17 +1000

    queries added

---
+++ jQuery/index.js
@@ -0,0 +1,27 @@
+
+// check whether jQuery is ready or not, if you put script tag at above we have to check whether jQuery loaded or not
+// $(document).ready(function(){
+//   $("h1").css("color","yellow");
+// });
+
+// add Class
+$("h1").addClass("big-title");
+
+// add more than one class
+//$("h1").addClass("big-title title");
+
+// check for the class
+//$("h1").hasClass("big-title");
+
+//change the defalut text to defined one
+//$("h1").text("Click me or not");
+$("h1").text("Bye");
+
+//to change the html tag
+$("button").html("");
+
+// remove class
+//$("h1").removeClass("big-title");
+
+// change the h1 title to color green
+//$("h1").css("color", "green");
